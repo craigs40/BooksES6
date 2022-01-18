33d0ea4aaafc36065e362fc5bba4a8947a0e2c77
Shay Craig <71286979+craigs40@users.noreply.github.com>
2022-01-18 12:46:59 -0500

    Fix error

--- modules/module.js
+++ modules/module.js
@@ -2,5 +2,5 @@ import { DateTime } from 'luxon';
 
 setInterval(() => {
   const dt = DateTime.now();
-  document.getElementById("date").textContent = dt.toLocaleString(DateTime.DATETIME_FULL_WITH_SECONDS);
+  document.getElementById('date').textContent = dt.toLocaleString(DateTime.DATETIME_FULL_WITH_SECONDS);
 }, 1000);
