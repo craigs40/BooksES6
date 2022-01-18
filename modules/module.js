5e0836facb804111003ad14e69665fb2cc7a4926
Shay Craig <71286979+craigs40@users.noreply.github.com>
2022-01-18 12:36:58 -0500

    Fix linter error

--- modules/module.js
+++ modules/module.js
@@ -2,4 +2,5 @@ import { DateTime } from 'luxon';
 
 setInterval(() => {
   const dt = DateTime.now();
-});
+  divDate.textContent = dt.toLocaleString(DateTime.DATETIME_FULL_WITH_SECONDS);
+}, 1000);
